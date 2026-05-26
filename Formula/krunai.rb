5e5a208ac9aa2860b980f685dcdad01af14b14e2
Sergio Lopez <slp@redhat.com>
2026-05-26 16:46:31 +0200

    krunai: fix bindgen clang dependency
    Closes #43.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>
    Signed-off-by: Sergio López <511389+slp@users.noreply.github.com>

--- Formula/krunai.rb
+++ Formula/krunai.rb
@@ -5,6 +5,7 @@ class Krunai < Formula
   sha256 "d19457adcb024beff9a5d103cc8aac59b1ccae397fef46e9c2eb93f2f330922c"
   license "Apache-2.0"
 
+  depends_on "llvm" => :build
   depends_on "rust" => :build
   # We depend on libkrun, which only supports Hypervisor.framework on arm64
   depends_on arch: :arm64
@@ -14,6 +15,7 @@ class Krunai < Formula
   depends_on "qemu"
 
   def install
+    ENV["LIBCLANG_PATH"] = Formula["llvm"].opt_lib.to_s
     homebrew_lib = ENV["HOMEBREW_PREFIX"] + "/lib"
     system "make"
     MachO::Tools.add_rpath("target/release/krunai", homebrew_lib)
