e2105506456a5ca83ab689dec6df5e93ab4ee27a
Sergio Lopez <slp@redhat.com>
2026-06-08 17:35:02 +0200

    libkrunfw: update to version 5.5.0
    Closes #44.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>
    Signed-off-by: Sergio López <511389+slp@users.noreply.github.com>

--- Formula/libkrunfw.rb
+++ Formula/libkrunfw.rb
@@ -1,16 +1,10 @@
 class Libkrunfw < Formula
   desc "Dynamic library bundling a Linux kernel in a convenient storage format"
   homepage "https://github.com/containers/libkrunfw"
-  url "https://github.com/containers/libkrunfw/releases/download/v5.3.0/libkrunfw-prebuilt-aarch64.tgz"
-  version "5.3.0"
-  sha256 "6a3d50ae9c14fa2be4b4b8f1027bde5aa4bc524b5685db8e8212b487cab3d198"
+  url "https://github.com/containers/libkrunfw/releases/download/v5.5.0/libkrunfw-prebuilt-aarch64.tgz"
+  version "5.5.0"
+  sha256 "5bfae6efee63dbdf04a8fac2a69d772d9f900af2f54c4429b4acdfd6d86b9979"
   license all_of: ["GPL-2.0-only", "LGPL-2.1-only"]
-
-  bottle do
-    root_url "https://raw.githubusercontent.com/slp/homebrew-krun/master/bottles"
-    sha256 cellar: :any, arm64_tahoe:   "601a7111a4704a5610cd63d0093b37ec999035310da176cba49d4d00339f1a1c"
-    sha256 cellar: :any, arm64_sequoia: "28dd39bdb696940d2df95d13bb648317143b35177e6991375e43155f4a9c8fae"
-  end
 
   # libkrun, our only consumer, only supports Hypervisor.framework on arm64
   depends_on arch: :arm64
