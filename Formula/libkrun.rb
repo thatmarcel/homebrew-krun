d99875a7d60399268c8fb46df18f0a7b6fd7f32b
Sergio Lopez <slp@redhat.com>
2026-06-24 16:54:18 +0200

    libkrun: update to version 1.19.1
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- Formula/libkrun.rb
+++ Formula/libkrun.rb
@@ -1,15 +1,9 @@
 class Libkrun < Formula
   desc "Dynamic library providing KVM-based process isolation capabilities"
   homepage "https://github.com/libkrun/libkrun"
-  url "https://github.com/containers/libkrun/archive/refs/tags/v1.19.0.tar.gz"
-  sha256 "832e76e93f1ea7a41e5c763a9710acf42d3d54f628015c1255f115a4b7ef2a06"
+  url "https://github.com/containers/libkrun/archive/refs/tags/v1.19.1.tar.gz"
+  sha256 "303d765096fc28a7c31184a262fa8a9ed759c1ab6f156fe9b037b2d3f4931e25"
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://github.com/slp/homebrew-krun/releases/download/libkrun-1.19.0"
-    sha256 cellar: :any, arm64_tahoe:   "ea7d0ffb5193aa108a95400a622474cef88f0b8da68b0660b15dba3bc5b0958c"
-    sha256 cellar: :any, arm64_sequoia: "f58f1befc5cd5c8ae1ae260c94523820975ae9da7c983497a9242413e156f626"
-  end
 
   depends_on "lld" => :build
   depends_on "rust" => :build
